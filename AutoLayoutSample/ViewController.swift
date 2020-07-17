94f8e431165e23fce6c5752285e0ee33ad02db2b
artemiy <->
2020-07-17 11:29:16 +0300

    Done

--- AutoLayoutSample/ViewController.swift
+++ AutoLayoutSample/ViewController.swift
@@ -26,14 +26,45 @@ class ViewController: UIViewController {
         
         view.addSubview(squareView)
         view.addSubview(rectangleView)
-    }
-    
-    override func viewWillLayoutSubviews() {
+        
         configureLayout()
     }
     
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        coordinator.animate(alongsideTransition: { [weak self] _ in
+            guard let self = self else { return }
+            self.configureLayout()
+        })
+    }
+    
+    
     
     func configureLayout() {
+        guard let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation else { return }
+        view.removeConstraints(view.constraints)
+        squareView.removeConstraints(squareView.constraints)
+        rectangleView.removeConstraints(rectangleView.constraints)
+        if orientation.isPortrait {
+            addPortraitConstraints()
+        } else if orientation.isLandscape {
+            addLanscapeConstraints()
+        }
+    }
+    
+    func addLanscapeConstraints() {
+        squareView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        squareView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        rectangleView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+        rectangleView.leadingAnchor.constraint(equalTo: squareView.trailingAnchor, constant: 20).isActive = true
+        rectangleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20).isActive = true
+        rectangleView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    func addPortraitConstraints() {
         squareView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         squareView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         squareView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -43,6 +74,6 @@ class ViewController: UIViewController {
         rectangleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100).isActive = true
         rectangleView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
         rectangleView.topAnchor.constraint(equalTo: squareView.bottomAnchor, constant: 40).isActive = true
-        
     }
+    
 }
